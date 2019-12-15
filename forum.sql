5e15f2eca61b6cb70af2c4801e69f75f61ff2287
unknown <2900019381@qq.com>
2019-12-15 21:32:07 +0800

    forum.sql

---
+++ forum.sql
@@ -0,0 +1,64 @@
+DROP DATABASE forum;
+CREATE DATABASE forum;
+
+USE forum;
+DROP TABLE IF EXISTS user;
+CREATE TABLE user(
+    id VARCHAR(20),
+    password VARCHAR(20),
+    nickname VARCHAR(20),
+    gender VARCHAR(5),
+    age SMALLINT,
+    description VARCHAR(200),
+    avatar VARCHAR(200),
+    PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS post;
+CREATE TABLE post(
+    id INT,
+    title VARCHAR(50),
+    content VARCHAR(200),
+    uid VARCHAR(20),
+    nickname VARCHAR(20),
+    avatar VARCHAR(200)
+    PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS talkinfo;
+CREATE TABLE talkinfo(
+    id INT,
+    content VARCHAR(200),
+    senderid VARCHAR(20),
+    receiverid VARCHAR(20),
+    receivername VARCHAR(20),
+    sendtime BIGINT,
+    senderavatar VARCHAR(200),
+    receiveravatar VARCHAR(200)
+    PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS postreply;
+CREATE TABLE postreply(
+    id INT,
+    uid VARCHAR(20),
+    nickname VARCHAR(20),
+    avatar VARCHAR(200),
+    publishtime BIGINT,
+    directreply SMALLINT,
+    replyto INT,
+    postgroup INT,
+    PRIMARY KEY(id)
+);
+
+DROP TABLE if EXISTS friend;
+CREATE TABLE friend(
+    id INT,
+    oneid VARCHAR(20),
+    twoid VARCHAR(20),
+    onename VARCHAR(20),
+    twoname VARCHAR(20),
+    oneavatar VARCHAR(200),
+    twoavatar VARCHAR(200),
+    PRIMARY KEY(id)
+);
